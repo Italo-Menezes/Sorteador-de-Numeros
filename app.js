e204bdd249a02d3a078c0c97d0d18b1596be1f9e
Italo Sampaio <italosma201813@gmail.com>
2025-09-01 16:30:21 -0300

    Exibindo os números sorteados

--- app.js
+++ app.js
@@ -8,14 +8,24 @@ function sortear() {
 
     for(let i = 0; i < quantidade; i++ ) { 
         numero = obterNumeroAleatorio (de, ate);
+
+        while(sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        };
+
         sorteados.push(numero);
     }
     
+    let resultado = document.getElementById('resultado'); 
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 
 }
-
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random()* (max - min + 1 )) + min ; 
 }
+
+
+
+
 
