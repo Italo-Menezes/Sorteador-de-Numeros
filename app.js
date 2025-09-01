7a114dfcbec3c6e22ca136468babacbca90fc9a3
Italo Sampaio <italosma201813@gmail.com>
2025-09-01 16:59:29 -0300

    proteção no total de números sorteados

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ function sortear() {
     
     let sorteados = []; 
     let numero; 
+    
+    //Garante que nenhum campo venha vazio
+    if (isNaN(quantidade) ||quantidade == 0  || isNaN(de) || isNaN(ate)) {
+        alert('Preencha todos os campos corretamente');
+        return;
+    }
+
+    // garante que não peça mais números do que o intervalo permite
+    if (quantidade > (ate - de + 1)) {
+        alert("Quantidade maior que o intervalo de números disponíveis!");
+        return;
+    }
+
+    
 
     for(let i = 0; i < quantidade; i++ ) { 
         numero = obterNumeroAleatorio (de, ate);
@@ -18,12 +32,32 @@ function sortear() {
     
     let resultado = document.getElementById('resultado'); 
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
 
 }
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random()* (max - min + 1 )) + min ; 
 }
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) { 
+        botao.classList.remove('container__botao-desabilitado')
+        botao.classList.add('container__botao')
+    } else { 
+        botao.classList.remove('container__botao')
+        botao.classList.add('container__botao-desabilitado')
+    }
+ }
+ 
+ function reiniciar() {
+    document.getElementById('quantidade').value= '';
+    document.getElementById('de').value= '';
+    document.getElementById('ate').value= '';
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+    alterarStatusBotao();
+ }
 
 
 
